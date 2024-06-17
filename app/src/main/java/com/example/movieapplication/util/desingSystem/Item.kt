bfa2fb52950cf8af8d5f9fa3ac681c86a73c2755
spyro <spyrostsipas7@gmail.com>
2024-06-17 20:22:44 +0300

    Add AutoSwipeImage

---
+++ app/src/main/java/com/example/movieapplication/util/desingSystem/Item.kt
@@ -0,0 +1,13 @@
+package com.example.movieapplication.util.desingSystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.movieapplication.main.domain.models.Media
+
+@Composable
+fun Item(
+    media: Media,
+    onMediaClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+}
